37b070d7e7784f94030e396822206ee4e7f02ecb
Brenno Ribeiro <brenno.brenno.ribeiro.ribeiro@gmail.com>
2018-04-16 00:24:52 -0400

    Added Pull-up View

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Brenno Ribeiro on 4/16/18.
+//  Copyright © 2018 Brenno Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
